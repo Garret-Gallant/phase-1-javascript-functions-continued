d33691457f6a192369b52319284e8ea8d3233236
Garret Gallant <gallanttechnological@gmail.com>
2022-06-28 16:35:19 -0600

    lesson complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(string = `roller-skate`){
+    return(`This Saturday, I want to ${string}!`);
+}
+
+function mondayWork(activity = `go to the office`){
+    return (`This Monday, I will ${activity}.`);
+}
+
+function wrapAdjective(flairString = "*") {
+    return function innerFunction (innerString = `special`){
+        return (`You are ${flairString}${innerString}${flairString}!`);
+    }
+}
